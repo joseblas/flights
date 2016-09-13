137601cb499f51ae3768c06bdd24dc8de8256be7
Enda Quigley <endaquigley@gmail.com>
2016-09-13 10:44:42 +0100

    add loading state to header

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import loading from './images/loading.svg';
 import './css/header.css';
 
-const Header = () => {
-  return (
-    <header className="main-header">
-      <svg className="main-header__logo" viewBox="0 0 100 18">
-        <use xmlnsXlink="http://www.w3.org/1999/xlink" xlinkHref="#ryanair-logo" fill="white"></use>
-      </svg>
-    </header>
-  )
+class Header extends Component {
+
+  render() {
+
+    const { booking, flights } = this.props;
+    const showLoader = (booking.isFetching || flights.isFetching);
+
+    return (
+      <header className="main-header">
+        <svg className="main-header__logo" viewBox="0 0 100 18">
+          <use xmlnsXlink="http://www.w3.org/1999/xlink" xlinkHref="#ryanair-logo" fill="white"></use>
+        </svg>
+        <div className="main-header__loading">
+          { showLoader && <img src={ loading } alt="Loading Data"/> }
+        </div>
+      </header>
+    );
+  }
+
 }
 
-export default Header;
+const mapStateToProps = (state) => {
+  return {
+    booking: state.booking,
+    flights: state.flights
+  }
+}
+
+export default connect(mapStateToProps)(Header);
